fae27df8878f37993a8a3e2a2c58b0c0c07dff24
SourceLInk <limianqi1995@gmail.com>
2016-10-22 10:14:21 +0800

    i/o inout

---
+++ Part3/efficiency.c
@@ -0,0 +1,29 @@
+#include "apue.h"
+
+#define BuffSize	4096
+
+int
+main(void)
+{
+	int n;
+	char buf[BuffSize];
+
+	while((n = read(STDIN_FILENO,buf,BuffSize)) > 0)
+	{
+		if(write(STDOUT_FILENO,buf,n) != n)
+			{
+				err_sys("write error ");
+			}
+	}
+	
+	if(n < 0)
+	{
+		err_sys("read error");
+
+	}
+	
+	exit(0);
+}
+
+
+
